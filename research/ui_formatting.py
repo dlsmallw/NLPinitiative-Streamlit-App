bc315004cc36f3e3f8c86d49c33b338035cae498
dlsmallw <dlsmallw@asu.edu>
2025-03-01 12:58:39 +0900

    Task-272 Uncommented code

--- research/ui_formatting.py
+++ research/ui_formatting.py
@@ -4,41 +4,41 @@ import numpy as np
 import time
 
 
-# ## Sidebar Usage:
-# ex_select_box = st.sidebar.selectbox(
-#     'Favorite Music Genre:',
-#     ('Rock', 'Reggae', 'Bluegrass')
-# )
+## Sidebar Usage:
+ex_select_box = st.sidebar.selectbox(
+    'Favorite Music Genre:',
+    ('Rock', 'Reggae', 'Bluegrass')
+)
 
-# ex_slider = st.sidebar.slider(
-#     'Age Range:',
-#     0, 100, (25, 75)
-# )
+ex_slider = st.sidebar.slider(
+    'Age Range:',
+    0, 100, (25, 75)
+)
 
-# ## Column Usage:
-# lc, rc = st.columns(2)
-# ## Columns like sidebars:
-# lc.button('Example - Left Column Button')
+## Column Usage:
+lc, rc = st.columns(2)
+## Columns like sidebars:
+lc.button('Example - Left Column Button')
 
-# ## Using with blocks:
-# with rc:
-#     choice = st.radio(
-#         'Favorite Color:',
-#         ("Red", "Blue", "Green", "Magenta"))
-#     st.write(f"{choice} is your favorite color!" if choice != 'Magenta' else f'{choice} is not a natural color!')
+## Using with blocks:
+with rc:
+    choice = st.radio(
+        'Favorite Color:',
+        ("Red", "Blue", "Green", "Magenta"))
+    st.write(f"{choice} is your favorite color!" if choice != 'Magenta' else f'{choice} is not a natural color!')
 
-# ## Progress Bars:
-# 'Loading...'
+## Progress Bars:
+'Loading...'
 
-# percentage = st.empty()
-# bar = st.progress(0)
+percentage = st.empty()
+bar = st.progress(0)
 
-# for i in range(100):
-#     # Update the progress bar with each iteration.
-#     percentage.text(f'{i+1}%')
-#     bar.progress(i + 1)
-#     time.sleep(0.1)
-# 'Loading complete!'
+for i in range(100):
+    # Update the progress bar with each iteration.
+    percentage.text(f'{i+1}%')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+'Loading complete!'
 
 ## Sessions:
 if "counter" not in st.session_state:
